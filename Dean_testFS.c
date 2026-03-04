cb17e019ea4c932f96d18db73a70b2970de3c70e
Dean <106890173+Deanosaur666@users.noreply.github.com>
2026-03-03 16:09:31 -0800

    eh

--- Dean_testFS.c
+++ Dean_testFS.c
@@ -1,24 +1,114 @@
 #include <stdio.h>
 #include "Dean_libFS.h"
+#include <string.h>
+#include <ncurses.h>
+#include <unistd.h>
+#include <linux/limits.h>
+#include <dirent.h>
+
+#define C_BLACK 30
+#define C_RED 31
+#define C_GREEN 32
+#define C_YELLOW 33
+#define C_BLUE 34
+#define C_MAGENTA 35
+#define C_CYAN 36
+#define C_WHITE 37
+
+#define C_BG(c) (c + 10)
+#define C_BRIGHT(c) (c + 60)
+
+void cursorToHome() {
+    // Write the sequence for clearing the display:
+    // \x1B[2J - Clears the visible window
+    // \x1B[3J - Clears the scroll back
+    // \x1B[1;1H - Move cursor back to its home coordinates (top left)
+    fputs("\x1B[3J\x1B[1;1H", stdout);  
+    fflush(stdout);
+}
+
+void setColor(int c) {
+    printf("\x1B[%dm", c);
+}
+
+int getFiles(char * names[255], char * types, int count, int offset) {
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(".");
+    int i = 0;
+    if(d) {
+        while((dir = readdir(d)) != NULL) {
+            if(offset > 0)
+                offset --;
+            else if(dir->d_type == DT_DIR || 
+                    dir->d_type == DT_REG) {
+                names[i] = dir->d_name;
+                types[i] = dir->d_type;
+                // type could be DT_DIR for directory
+                // or DT_REG for regular
+                i ++;
+            }
+        }
+        names[i] = "";
+        types[i] = '\0';
+    }
+
+    return i;
+}
 
 int main() {
-    // create file with fileCreate:
-    // Dean_Introduction.txt
 
-    // open file with fileOpen
-
-    // write introduction to file with fileWrite
-    // at least 3-4 sentences, 2 paragraphs
-
-    // close file with fileClose
-
-    // open file again
-    // read from file with fileRead
-    // print contents
-
-    // close file again
-
+    // OPTIONS
+    // create file
+    // write to file
+    // print file contents
     // delete file
 
-    // use promts??? Menu-driven approach???
+    char * options[] = {
+        "CREATE",
+        "DISPLAY",
+        "EDIT",
+        "DELETE",
+    };
+
+    //char cwd[PATH_MAX];
+    //getcwd(cwd, sizeof(cwd));
+
+    initscr();
+    clear();
+    noecho();
+    cbreak();
+    keypad(stdscr, true);
+
+    refresh();
+    cursorToHome();
+    setColor(C_BG(C_BRIGHT(C_RED)));
+    printf("Hey hey hey.");
+    setColor(0);
+    printf("\nHey hey hey.\n");
+
+    while(1) {
+        int key = getch();
+        if(key == KEY_UP) {
+            printf("UP!\n");
+            cursorToHome();
+        }
+        else if(key == KEY_DOWN) {
+            printf("DOWN!\n");
+            cursorToHome();
+        }
+        else if(key == 'q') {
+            break;
+        }
+        setColor(C_BG(C_BRIGHT(C_RED)));
+        printf("Hey hey hey.");
+        setColor(0);
+        printf("\nHey hey hey.\n");
+        refresh();
+        cursorToHome();
+    }
+
+    endwin();
+
+    return 0;
 }
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: Dean_testFS
 
 Dean_testFS : Dean_testFS.c Dean_libFS.c Dean_libFS.h
-	@gcc Dean_testFS.c Dean_libFS.c -o Dean_testFS -Wall
+	@gcc Dean_testFS.c Dean_libFS.c -o Dean_testFS -Wall -lncurses
 
 clean:
 	@rm -f Dean_testFS
